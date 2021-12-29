3285db9e2484a9149daef4f088a5188b17924e3c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-29 11:04:14 +0100

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   const initialState = [];
   const [blogs, setBlogs] = useState(initialState);
   const [isLoading, setIsLoading] = useState(true)
+  // const [error, setError] = useState(null)
   
   
   // const handleDelete = (id) => {
@@ -16,15 +17,22 @@ const Home = () => {
     // to get this endpoint, run the following on the command line: npx json-server --watch data/db.json --port3000
     fetch("http://localhost:3000/blogs")
     .then(Response => {
+      // if (!Response.ok) {
+      //   throw Error('data did not fetch')
+      // }
       return Response.json()
     })
     .then(data => {
      setBlogs(data)
      setIsLoading(false)
     })
+    // .catch(err => {
+    //   setError(err.message)
+    // })
   },[])
   return (
     <div className="home">
+      {/* {error && <div>{error}</div>} */}
       {isLoading && <div> Loading... </div>}
       {blogs && <Blogs blogsProp={blogs} titleProp="All Blogs" />}
       {/* {blogs && <Blogs blogsProp={blogs.filter((blog) => blog.author === 'Mikel')} titleProp="Mikel's Blogs"/>} */}
