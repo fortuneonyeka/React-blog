6659504b4c3a294a375c95e9ea009b26f53b0fd8
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-29 11:49:40 +0100

    create a custom useFetch hook

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+
+
+const useFetch =(url) => {
+  const initialState = [];
+  const [data, setData] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    // to get this endpoint, run the following on the command line: npx json-server --watch data/db.json --port3000
+    fetch(url)
+      .then((Response) => {
+        if (!Response.ok) {
+          throw Error("data did not fetch");
+        }
+        return Response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return {data, isLoading,error}
+}
+
+export default useFetch
