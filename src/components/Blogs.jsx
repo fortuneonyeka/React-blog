15e4e8dfbdef678e67fc158171f7fca25eb2aae5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-29 10:31:37 +0100

    removed delete blog button

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,14 +1,17 @@
 // import { useState } from "react";
-const Blogs = ({blogsProp, titleProp, handleDelete}) => {
+const Blogs = ({blogsProp, titleProp}) => {
   
   return (
-    <div className='blogs'>
-      <h1 style={{ marginBottom: '20px', marginTop: '25px' }}>{titleProp}</h1>
+    <div
+      className="blogs"
+      style={{ padding: "20px", padding: "20px", lineHeight: "40px" }}
+    >
+      <h1 style={{ marginBottom: "20px", marginTop: "25px" }}>{titleProp}</h1>
       {blogsProp.map((blog) => (
-        <div className='blog-preview' key={blog.id}>
+        <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Wrtiten by: {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete</button>
+          {/* <button onClick={() => handleDelete(blog.id)}>Delete</button> */}
           <h2>{blog.body} </h2>
         </div>
       ))}
