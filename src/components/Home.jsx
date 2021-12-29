9c87657d3302c4b695bab585198bbbe4c40d5d08
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-12-29 11:06:33 +0100

    fetching error handling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   const initialState = [];
   const [blogs, setBlogs] = useState(initialState);
   const [isLoading, setIsLoading] = useState(true)
-  // const [error, setError] = useState(null)
+  const [error, setError] = useState(null)
   
   
   // const handleDelete = (id) => {
@@ -17,23 +17,23 @@ const Home = () => {
     // to get this endpoint, run the following on the command line: npx json-server --watch data/db.json --port3000
     fetch("http://localhost:3000/blogs")
     .then(Response => {
-      // if (!Response.ok) {
-      //   throw Error('data did not fetch')
-      // }
+      if (!Response.ok) {
+        throw Error('data did not fetch')
+      }
       return Response.json()
     })
     .then(data => {
      setBlogs(data)
      setIsLoading(false)
     })
-    // .catch(err => {
-    //   setError(err.message)
-    // })
+    .catch(err => {
+      setError(err.message)
+    })
   },[])
   return (
     <div className="home">
-      {/* {error && <div>{error}</div>} */}
       {isLoading && <div> Loading... </div>}
+      {error && <div>{error}</div>}
       {blogs && <Blogs blogsProp={blogs} titleProp="All Blogs" />}
       {/* {blogs && <Blogs blogsProp={blogs.filter((blog) => blog.author === 'Mikel')} titleProp="Mikel's Blogs"/>} */}
     </div>
