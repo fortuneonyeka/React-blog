cd8958326815ed80262adce572f9c5d7f5373737
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-04 10:12:44 +0100

    implement DELETE request

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -8,7 +8,13 @@ const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isLoading } = useFetch("http://localhost:3000/blogs/" + id);
 
-  
+  const handleDelete = () => {
+    fetch("http://localhost:3000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -19,7 +25,7 @@ const BlogDetails = () => {
           <h2 className="blog-detail-title">{blog.title}</h2>
           <p className="blog-detail-author">Written By : {blog.author}</p>
           <div>{blog.body}</div>
-         
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
